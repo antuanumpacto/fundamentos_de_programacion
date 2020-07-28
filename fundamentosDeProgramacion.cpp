029a2a964c49983cdc8f4998beafae654848896a
unknown <antuan_1996@hotmail.com>
2020-07-27 21:39:59 -0500

    agregar informacion de comandos status y clone

--- fundamentosDeProgramacion.cpp
+++ fundamentosDeProgramacion.cpp
@@ -18,6 +18,20 @@ int main()
         << "|                  Antuan Barahona                      |\n"
         << "---------------------------------------------------------";
 
+    //Manejo de terminal o consola
+    //Iniciar la terminal: tecla windows + r y escribir cmd
+    //cd = ingresar a carpetas
+    //cd .. = salir de carpeta
+    //Git
+    //Comandos git
+    //git init = inicia un repositorio vacio
+    //git add * = agregar todo los archivos al repositorio (*), en caso de querer subir un archivo puntual cambiar el * por el nombre del archivo
+    //git commit - m "text" = agregar informacion a lo qeu vamos a subir
+    //git remote add origin https://github.com/antuanumpacto/fundamentos_de_programacion.git: agregar origin
+    //git push -u origin master: push es pra subir cambios
+    //git clone url: copiar el proyecto
+    //git status: ver archivos manipulados
+
 
     //Se retorna 0 porque cero no tiene valor y la funcion main especifica que se retorne un entero
     return 0;
