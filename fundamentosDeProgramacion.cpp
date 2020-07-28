f5cb83fdfda4d764d0b4070b8865dea78b973cf2
unknown <antuan_1996@hotmail.com>
2020-07-27 21:35:45 -0500

    Imprimir textos en pantalla con c++

---
+++ fundamentosDeProgramacion.cpp
@@ -0,0 +1,26 @@
+//Librerias,importacion etc etc
+#include <iostream>
+using namespace std;
+
+//Metodo principal, es el primero en ejecutarse
+int main()
+{
+    //Imprimir texto en pantalla se utiliza cout <<
+    /*
+        \a Alarma
+        \n Salto de linea
+        \t Tabulador
+        \\ Escapar \
+        \' Escapar '
+        \" Escapar "
+    */
+    cout <<"---------------------------------------------------------\n"
+        << "|                  Antuan Barahona                      |\n"
+        << "---------------------------------------------------------";
+
+
+    //Se retorna 0 porque cero no tiene valor y la funcion main especifica que se retorne un entero
+    return 0;
+}
+
+
